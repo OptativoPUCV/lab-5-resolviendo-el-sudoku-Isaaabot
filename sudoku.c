0dbff0d4943464c5fcfe3c5e0e49b07a3d7512c8
Your Name <you@example.com>
2025-05-14 23:19:22 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -63,7 +63,7 @@ La salida del código con el estado de la figura es (submatriz del centro):
 */
 int is_valid(Node *n)
 {
-  for (int f = 0 ; f < 10 ; f++)
+  for (int f = 0 ; f < 9 ; f++)
   {
     int vistos[10] = {0} ;
 
@@ -79,7 +79,7 @@ int is_valid(Node *n)
     
   }
 
-  for (int c = 0 ; c < 10 ; c++)
+  for (int c = 0 ; c < 9 ; c++)
   {
     int vistos[10] = {0} ;
 
@@ -104,11 +104,11 @@ int is_valid(Node *n)
     printf("%d ",nodo->sudo[i][j]);
     if(p%3 == 2) printf("\n");
   */
-  for (int k = 0 ; k < 10 ; k++)
+  for (int k = 0 ; k < 9 ; k++)
   {
     int vistos[10] = {0} ;
 
-    for (int p = 0 ; p < 10 ; p++)
+    for (int p = 0 ; p < 9 ; p++)
     {
       int f = 3 * (k / 3) + (p / 3) ;
       int c = 3 * (k % 3) + (p % 3) ;
